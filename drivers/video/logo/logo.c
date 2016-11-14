afe642d5f77de69ea44820a9acd9819c5a469947
Eran Matityahu <eran.m@variscite.com>
2016-11-14 16:18:23 +0200

    video: logo: Add a Variscite Linux logo
    Signed-off-by: Eran Matityahu <eran.m@variscite.com>

--- drivers/video/logo/logo.c
+++ drivers/video/logo/logo.c
@@ -78,6 +78,10 @@ const struct linux_logo * __ref fb_find_logo(int depth)
 		/* Generic Linux logo */
 		logo = &logo_linux_clut224;
 #endif
+#ifdef CONFIG_LOGO_VARISCITE_CLUT224
+		/* Variscite Linux logo */
+		logo = &logo_variscite_clut224;
+#endif
 #ifdef CONFIG_LOGO_BLACKFIN_CLUT224
 		/* Blackfin Linux logo */
 		logo = &logo_blackfin_clut224;
