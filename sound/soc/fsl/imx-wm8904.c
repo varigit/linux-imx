9f9418280934d04464b7bd06b06596e458f76139
Pierluigi Passaro <pierluigi.p@variscite.com>
2018-05-16 22:57:26 +0200

    imx8m-var-dart: rework wm8904 management

--- sound/soc/fsl/imx-wm8904.c
+++ sound/soc/fsl/imx-wm8904.c
@@ -21,9 +21,6 @@
 #include <sound/soc-dapm.h>
 #include "../codecs/wm8904.h"
 
-#undef __USE_LATE_PROBE__
-#undef __USE_WM8904_FLL__
-
 struct imx_priv {
 	struct platform_device *pdev;
 	struct snd_soc_card card;
@@ -37,52 +34,14 @@ static const struct snd_soc_dapm_widget imx_wm8904_dapm_widgets[] = {
 	SND_SOC_DAPM_LINE("Line In Jack", NULL),
 };
 
-#ifdef __USE_LATE_PROBE__
-static int imx_wm8904_late_probe(struct snd_soc_card *card)
-{
-	struct imx_priv *priv = snd_soc_card_get_drvdata(card);
-	int ret;
-
-	ret = clk_prepare_enable(priv->codec_clk);
-	if (ret) {
-		dev_err(card->dev, "Failed to enable MCLK: %d\n", ret);
-		return ret;
-	}
-
-	return 0;
-}
-#endif
-
 static int imx_hifi_hw_params(struct snd_pcm_substream *substream,
 				     struct snd_pcm_hw_params *params)
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
 	struct snd_soc_dai *codec_dai = rtd->codec_dai;
-	struct snd_soc_card *card = rtd->card;
-	struct device *dev = card->dev;
-	struct imx_priv *priv = snd_soc_card_get_drvdata(card);
-	unsigned int fmt;
+	struct imx_priv *priv = snd_soc_card_get_drvdata(rtd->card);
 	int ret = 0;
 
-	fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF | SND_SOC_DAIFMT_CBM_CFM;
-
-	/* set codec DAI configuration */
-	ret = snd_soc_dai_set_fmt(codec_dai, fmt);
-	if (ret) {
-		dev_err(dev, "failed to set codec dai fmt: %d\n", ret);
-		return ret;
-	}
-
-	priv->clk_frequency = clk_get_rate(priv->codec_clk);
-#ifndef __USE_WM8904_FLL__
-	ret = snd_soc_dai_set_sysclk(codec_dai, WM8904_CLK_MCLK,
-					priv->clk_frequency, SND_SOC_CLOCK_IN);
-	if (ret) {
-		dev_err(card->dev, "failed to set codec sysclk: %d\n", ret);
-		return ret;
-	}
-#else
 	ret = snd_soc_dai_set_pll(codec_dai, WM8904_FLL_MCLK, WM8904_FLL_MCLK,
 		priv->clk_frequency, params_rate(params) * 256);
 	if (ret < 0) {
@@ -98,35 +57,13 @@ static int imx_hifi_hw_params(struct snd_pcm_substream *substream,
 	ret = snd_soc_dai_set_sysclk(codec_dai, WM8904_CLK_FLL,
 			0, SND_SOC_CLOCK_IN);
 	if (ret < 0) {
-		pr_err("%s -failed to set wm8904 SYSCLK\n", __func__);
+		pr_err("%s - failed to set wm8904 SYSCLK\n", __func__);
 		return ret;
 	}
 
-#endif
-	/* set cpu DAI configuration */
-	ret = snd_soc_dai_set_fmt(cpu_dai, fmt);
-	if (ret) {
-		dev_err(dev, "failed to set cpu dai fmt: %d\n", ret);
-		return ret;
-	}
-
-	ret = snd_soc_dai_set_sysclk(cpu_dai, 0, 0, SND_SOC_CLOCK_IN);
-	if (ret) {
-		dev_err(dev, "failed to set cpu sysclk: %d\n", ret);
-		return ret;
-	}
-/*
-	ret = snd_soc_dai_set_tdm_slot(cpu_dai, 0, 0, 2,
-					params_physical_width(params));
-	if (ret) {
-		dev_err(dev, "failed to set cpu dai tdm slot: %d\n", ret);
-		return ret;
-	}
-*/
 	return ret;
 }
 
-#ifndef __USE_LATE_PROBE__
 static int imx_hifi_startup(struct snd_pcm_substream *substream)
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
@@ -151,23 +88,31 @@ static void imx_hifi_shutdown(struct snd_pcm_substream *substream)
 
 	clk_disable_unprepare(priv->codec_clk);
 }
-#endif
 
 static struct snd_soc_ops imx_hifi_ops = {
 	.hw_params = imx_hifi_hw_params,
-#ifndef __USE_LATE_PROBE__
 	.startup   = imx_hifi_startup,
 	.shutdown  = imx_hifi_shutdown,
-#endif
 };
 
-static struct snd_soc_dai_link imx_wm8904_dai[] = {
-	{
-		.name = "HiFi",
-		.stream_name = "HiFi",
-		.codec_dai_name = "wm8904-hifi",
-		.ops = &imx_hifi_ops,
-	},
+static struct snd_soc_dai_link imx_wm8904_dailink = {
+	.name = "WM8904",
+	.stream_name = "WM8904 PCM",
+	.codec_dai_name = "wm8904-hifi",
+	.dai_fmt = SND_SOC_DAIFMT_I2S
+		 | SND_SOC_DAIFMT_NB_NF
+		 | SND_SOC_DAIFMT_CBM_CFM,
+	.ops = &imx_hifi_ops,
+};
+
+static struct snd_soc_card imx_wm8904_card = {
+	.name = "imx_wm8904",
+	.owner = THIS_MODULE,
+	.dai_link = &imx_wm8904_dailink,
+	.num_links = 1,
+	.dapm_widgets = imx_wm8904_dapm_widgets,
+	.num_dapm_widgets = ARRAY_SIZE(imx_wm8904_dapm_widgets),
+	.fully_routed = true,
 };
 
 static int imx_wm8904_probe(struct platform_device *pdev)
@@ -200,7 +145,7 @@ static int imx_wm8904_probe(struct platform_device *pdev)
 
 	cpu_pdev = of_find_device_by_node(cpu_np);
 	if (!cpu_pdev) {
-		dev_err(&pdev->dev, "failed to find SAI platform device\n");
+		dev_err(&pdev->dev, "failed to find CPU DAI platform device\n");
 		ret = -EINVAL;
 		goto fail;
 	}
@@ -218,23 +163,16 @@ static int imx_wm8904_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "failed to get codec clk: %d\n", ret);
 		goto fail;
 	}
+	priv->clk_frequency = clk_get_rate(priv->codec_clk);
+	dev_dbg(&pdev->dev, "mclk frequency: %d\n", priv->clk_frequency);
 
-	priv->card.dai_link = imx_wm8904_dai;
+	imx_wm8904_dailink.codec_of_node = codec_np;
+	imx_wm8904_dailink.cpu_dai_name = dev_name(&cpu_pdev->dev);
+	imx_wm8904_dailink.cpu_of_node = cpu_np;
+	imx_wm8904_dailink.platform_of_node = cpu_np;
 
-	imx_wm8904_dai[0].codec_of_node	= codec_np;
-	imx_wm8904_dai[0].cpu_dai_name = dev_name(&cpu_pdev->dev);
-	imx_wm8904_dai[0].platform_of_node = cpu_np;
-	imx_wm8904_dai[0].dai_fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF | SND_SOC_DAIFMT_CBM_CFM;
-
-#ifdef __USE_LATE_PROBE__
-	priv->card.late_probe = imx_wm8904_late_probe;
-#endif
-	priv->card.num_links = 1;
+	memcpy(&priv->card, &imx_wm8904_card, sizeof(imx_wm8904_card));
 	priv->card.dev = &pdev->dev;
-	priv->card.owner = THIS_MODULE;
-	priv->card.dapm_widgets = imx_wm8904_dapm_widgets;
-	priv->card.num_dapm_widgets = ARRAY_SIZE(imx_wm8904_dapm_widgets);
-	priv->card.fully_routed = true,
 
 	ret = snd_soc_of_parse_card_name(&priv->card, "model");
 	if (ret)
