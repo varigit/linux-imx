5aa7f0cd12c2737711df48ecde05ee6b4a1e92dc
Martin Sperl <kernel@martin.sperl.org>
2018-07-02 20:15:07 +0000

    can: mcp25xxfd: Add Microchip mcp25xxfd CAN FD driver basics
    This patch adds basic driver support for the Microchip mcp2517fd
    CAN-FD controller.
    
    The mcp2517fd is capable of transmitting and receiving standard data
    frames, extended data frames, remote frames and Can-FD frames.
    The mcp2517fd interfaces with the host over SPI.
    
    This patch iprovides basic driver functionality:
    setting up clocks and the infrastructure for the can and gpio portions
    of the driver.
    
    Datasheet:
    * http://ww1.microchip.com/downloads/en/DeviceDoc/20005688A.pdf
    Reference manual:
    * http://ww1.microchip.com/downloads/en/DeviceDoc/20005678A.pdf
    Errata:
    * http://ww1.microchip.com/downloads/en/DeviceDoc/MCP2517FD-Silicon-Errata-and-Data-Sheet-Clarification-DS80000792A.pdf
    
    --
    Changelog:
      V4 -> V5: reorganisation of the patchset into smaller patches
                review of the whole driver code for better modularization
      V5 -> V6: Major refactoring as per feedback from Wilhelm Grandegger
                Fixing bugs reported by several other parties
                Split out of optimizations into separate patches
      V6 -> V7: added include linux/irqreturn.h (feedback by Eric Scholz)
                move can transmission into a separate patch to reduce
                  the size of the individual patches to make the linux-can
                  mailing-list happy...
      V7 -> V8: add support for mcp2518fd
                fixed mcp25xxfd_cmd_read_mask bug clearing bits
                allow setting SPI clock speeds via debugfs
                Incorprated Feedback by Marc Kleine-Budde
                  sparse fixes
                  makefile indenting
    
    Signed-off-by: Martin Sperl <kernel@martin.sperl.org>
    Signed-off-by: Eran Matityahu <eran.m@variscite.com>

---
+++ drivers/net/can/spi/mcp25xxfd/mcp25xxfd_int.c
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: GPL-2.0
+
+/* CAN bus driver for Microchip 25XXFD CAN Controller with SPI Interface
+ *
+ * Copyright 2019 Martin Sperl <kernel@martin.sperl.org>
+ */
+
+#include <linux/kernel.h>
+#include <linux/spi/spi.h>
+
+#include "mcp25xxfd_crc.h"
+#include "mcp25xxfd_ecc.h"
+#include "mcp25xxfd_int.h"
+#include "mcp25xxfd_priv.h"
+
+int mcp25xxfd_int_clear(struct mcp25xxfd_priv *priv)
+{
+	int ret;
+
+	ret = mcp25xxfd_ecc_clear_int(priv);
+	if (ret)
+		return ret;
+	ret = mcp25xxfd_crc_clear_int(priv);
+
+	return ret;
+}
+
+int mcp25xxfd_int_enable(struct mcp25xxfd_priv *priv, bool enable)
+{
+	/* error handling only on enable for this function */
+	int ret = 0;
+
+	/* if we enable clear interrupt flags first */
+	if (enable) {
+		ret = mcp25xxfd_int_clear(priv);
+		if (ret)
+			goto out;
+	}
+
+	ret = mcp25xxfd_crc_enable_int(priv, enable);
+	if (ret)
+		goto out;
+
+	ret = mcp25xxfd_ecc_enable(priv);
+	if (ret)
+		goto out_crc;
+
+	ret = mcp25xxfd_ecc_enable_int(priv, enable);
+	if (ret)
+		goto out_crc;
+
+	/* if we disable interrupts clear interrupt flags last */
+	if (!enable)
+		mcp25xxfd_int_clear(priv);
+
+	return 0;
+
+out_crc:
+	mcp25xxfd_crc_enable_int(priv, false);
+out:
+	return ret;
+}
