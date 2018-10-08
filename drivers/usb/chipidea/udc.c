76e8f4e3ee4edfff51319ba23cedf6399e7c4fc4
Peter Chen <peter.chen@nxp.com>
2018-10-08 17:43:45 +0800

    MLK-19850-6 usb: chipidea: introduce flag for enable USB PHY charger detection
    Some platforms may want to use USB PHY charger detection function
    when VBUS is there, add one flag for it. The user can enable it
    at firmware.
    
    Reviewed-by: Jun Li <jun.li@nxp.com>
    Signed-off-by: Peter Chen <peter.chen@nxp.com>

--- drivers/usb/chipidea/udc.c
+++ drivers/usb/chipidea/udc.c
@@ -2092,6 +2092,9 @@ int ci_usb_charger_connect(struct ci_hdrc *ci, int is_active)
 
 	pm_runtime_get_sync(ci->dev);
 
+	if (!(ci->platdata->flags & CI_HDRC_PHY_CHARGER_DETECTION))
+		goto out;
+
 	if (ci->usb_phy->charger_detect) {
 		usb_phy_set_charger_state(ci->usb_phy, is_active ?
 			USB_CHARGER_PRESENT : USB_CHARGER_ABSENT);
@@ -2101,6 +2104,7 @@ int ci_usb_charger_connect(struct ci_hdrc *ci, int is_active)
 		schedule_work(&ci->usb_phy->chg_work);
 	}
 
+out:
 	pm_runtime_put_sync(ci->dev);
 	return ret;
 }
