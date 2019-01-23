651b29a518bf9bd101ac7a823c30c091b2ffd3d1
Felix Radensky <felix.r@variscite.com>
2019-01-23 18:26:47 +0200

    sn65dsi83: Fix DSI mode flags
    Either MIPI_DSI_MODE_VIDEO_SYNC_PULSE or MIPI_DSI_MODE_VIDEO_BURST
    is required by i.MX8MM MIPI DSI bridge driver

--- drivers/gpu/drm/bridge/sn65dsi83/sn65dsi83_drv.c
+++ drivers/gpu/drm/bridge/sn65dsi83/sn65dsi83_drv.c
@@ -331,8 +331,7 @@ static int sn65dsi83_attach_dsi(struct sn65dsi83 *sn65dsi83)
 
     dsi->lanes = sn65dsi83->brg->num_dsi_lanes;
     dsi->format = MIPI_DSI_FMT_RGB888;
-    dsi->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_HSE |
-               MIPI_DSI_CLOCK_NON_CONTINUOUS;
+    dsi->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_SYNC_PULSE;
 
     ret = mipi_dsi_attach(dsi);
     if (ret < 0) {
