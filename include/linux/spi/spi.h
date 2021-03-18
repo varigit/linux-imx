8f770b80576d164e079e3060ad77efd09cf9e718
Martin Sperl <kernel@martin.sperl.org>
2021-03-18 12:30:13 -0700

    spi: core: allow reporting the effectivly used speed_hz for a  transfer
    Provide a means for the spi bus driver to report the effectively used
    spi clock frequency used for each spi_transfer.

--- include/linux/spi/spi.h
+++ include/linux/spi/spi.h
@@ -714,6 +714,9 @@ extern void spi_res_release(struct spi_controller *ctlr,
  * @delay_usecs: microseconds to delay after this transfer before
  *	(optionally) changing the chipselect status, then starting
  *	the next transfer or completing this @spi_message.
+ * @effective_speed_hz: the effective SCK-speed that was used to
+ *      transfer this transfer. Set to 0 if the spi bus driver does
+ *      not support it.
  * @transfer_list: transfers are sequenced through @spi_message.transfers
  * @tx_sg: Scatterlist for transmit, currently not for client use
  * @rx_sg: Scatterlist for receive, currently not for client use
@@ -801,7 +804,7 @@ struct spi_transfer {
 #define SPI_DELAY_UNIT_NSECS	1
 #define SPI_DELAY_UNIT_SCK	2
 	u32		speed_hz;
-
+	u32		effective_speed_hz;
 	struct list_head transfer_list;
 };
 
