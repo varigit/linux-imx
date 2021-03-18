488a6556d2db34f14fafe8e05d106984492f7319
Martin Sperl <kernel@martin.sperl.org>
2021-03-18 12:06:41 -0700

    spi: core: allow defining time that cs is deasserted
    For some SPI devices that support speed_hz > 1MHz the default 10 us delay
    when cs_change = 1 is typically way to long and may result in poor spi bus
    utilization.
    
    This patch makes it possible to control the delay at micro or nano second
    resolution on a per spi_transfer basis. It even allows an "as fast as
    possible" mode with:
        xfer.cs_change_delay_unit = SPI_DELAY_UNIT_NSECS;
        xfer.cs_change_delay = 0;
    
    The delay code is shared between delay_usecs and cs_change_delay for
    consistency and reuse, so in the future this change_delay_unit could also
    apply to delay_usec as well.
    
    Note that on slower SOCs/CPU actually reaching ns deasserts on cs is not
    realistic as the gpio overhead alone (without any delays added ) may
    already leave cs deasserted for more than 1us - at least on a raspberry pi.
    But at the very least this way we can keep it as short as possible.

--- drivers/spi/spi.c
+++ drivers/spi/spi.c
@@ -993,6 +993,52 @@ static int spi_map_msg(struct spi_controller *ctlr, struct spi_message *msg)
 	return __spi_map_msg(ctlr, msg);
 }
 
+static void _spi_transfer_delay_ns(u32 ns)
+{
+	if (!ns)
+		return;
+	if (ns <= 1000) {
+		ndelay(ns);
+	} else {
+		u32 us = DIV_ROUND_UP(ns, 1000);
+
+		if (us <= 10)
+			udelay(us);
+		else
+			usleep_range(us, us + DIV_ROUND_UP(us, 10));
+	}
+}
+
+static void _spi_transfer_cs_change_delay(struct spi_message *msg,
+					  struct spi_transfer *xfer)
+{
+	u32 delay = xfer->cs_change_delay;
+	u32 unit = xfer->cs_change_delay_unit;
+
+	/* return early on "fast" mode - for everything but USECS */
+	if (!delay && unit != SPI_DELAY_UNIT_USECS)
+		return;
+
+	switch (unit) {
+	case SPI_DELAY_UNIT_USECS:
+		/* for compatibility use default of 10us */
+		if (!delay)
+			delay = 10000;
+		else
+			delay *= 1000;
+		break;
+	case SPI_DELAY_UNIT_NSECS: /* nothing to do here */
+		break;
+	default:
+		dev_err_once(&msg->spi->dev,
+			"Use of unsupported delay unit %i, using default of 10us\n",
+			xfer->cs_change_delay_unit);
+		delay = 10000;
+	}
+	/* now sleep for the requested amount of time */
+	 _spi_transfer_delay_ns(delay);
+}
+
 /*
  * spi_transfer_one_message - Default implementation of transfer_one_message()
  *
@@ -1069,14 +1115,8 @@ static int spi_transfer_one_message(struct spi_controller *ctlr,
 		if (msg->status != -EINPROGRESS)
 			goto out;
 
-		if (xfer->delay_usecs) {
-			u16 us = xfer->delay_usecs;
-
-			if (us <= 10)
-				udelay(us);
-			else
-				usleep_range(us, us + DIV_ROUND_UP(us, 10));
-		}
+		if (xfer->delay_usecs)
+			_spi_transfer_delay_ns(xfer->delay_usecs * 1000);
 
 		if (xfer->cs_change) {
 			if (list_is_last(&xfer->transfer_list,
@@ -1084,7 +1124,7 @@ static int spi_transfer_one_message(struct spi_controller *ctlr,
 				keep_cs = true;
 			} else {
 				spi_set_cs(msg->spi, false);
-				udelay(10);
+				_spi_transfer_cs_change_delay(msg, xfer);
 				spi_set_cs(msg->spi, true);
 			}
 		}
