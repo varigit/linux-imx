a5b5ecfa3d4bddf66a8b26b1aa73002a8f7df470
Pierluigi Passaro <pierluigi.p@variscite.com>
2021-06-18 18:21:34 +0200

    Revert "pad-imx8qm: add defines for wakeup trigger types"
    This reverts commit db56514f6b8a3bc6a04d9c65270f09b8674b9a14.
    
    Signed-off-by: Pierluigi Passaro <pierluigi.p@variscite.com>

--- include/dt-bindings/pinctrl/pads-imx8qm.h
+++ include/dt-bindings/pinctrl/pads-imx8qm.h
@@ -957,14 +957,4 @@
 #define IMX8QM_COMP_CTL_GPIO_1V8_3V3_ENET_ENETB_PAD		IMX8QM_COMP_CTL_GPIO_1V8_3V3_ENET_ENETB		0
 #define IMX8QM_COMP_CTL_GPIO_1V8_3V3_ENET_ENETA_PAD		IMX8QM_COMP_CTL_GPIO_1V8_3V3_ENET_ENETA		0
 
-/*
- * Defines for the wakeup trigger types
- */
-#define SC_PAD_WAKEUP_OFF       0U    /* Off */
-#define SC_PAD_WAKEUP_CLEAR     1U    /* Clears pending flag */
-#define SC_PAD_WAKEUP_LOW_LVL   4U    /* Low level */
-#define SC_PAD_WAKEUP_FALL_EDGE 5U    /* Falling edge */
-#define SC_PAD_WAKEUP_RISE_EDGE 6U    /* Rising edge */
-#define SC_PAD_WAKEUP_HIGH_LVL  7U    /* High-level */
-
 #endif /* _IMX8QM_PADS_H */
