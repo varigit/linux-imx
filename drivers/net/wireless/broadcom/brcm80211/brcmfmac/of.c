e4e445bbf577a7a511bb80e58bdb771489298e4a
Felix Radensky <felix.r@variscite.com>
2021-06-23 12:28:23 +0300

    brcmfmac: Add support for setting regdomain via module parameter or DTS
    Add Laird code to set regulatory domain (country code) via module
    parameter or DTS property instead of using special firmware file.
    The following country codes are supported:
    
    US   LWB/LWB5   United States
    CA   LWB5       Canada (Use US for LWB)
    ETSI LWB/LWB5   All member countries of the European Union
    JP   LWB/LWB5   Japan
    AU   LWB5       Australia (use EU for LWB)
    NZ   LWB5       New Zealand (use EU for LWB)
    
    DTS configuration is preferred and will override the module parameter
    if both are present.
    
    The example of module parameter setting in /etc/modprobe.d/brcmfmac.conf
    
    options brcmfmac regdomain="US"
    
    The example of DTS setting:
    
    &usdhc1 {
            #address-cells = <1>;
            #size-cells = <0>;
    
    	...
    
            brcmf: bcrmf@1 {
                    reg = <1>;
                    compatible = "brcm,bcm4329-fmac";
    		laird,regdomain = "US";
            };
    };
    
    Replacing strlcpy() with strscpy():
    
    strlcpy() reads the entire source buffer first and returns the size of
    the source string, not the destination string, which can be accidentally
    misused [1].
    In an effort to remove strlcpy() completely [2], replace
    strlcpy() here with strscpy().
    
    [1] https://www.kernel.org/doc/html/latest/process/deprecated.html#strlcpy
    [2] https://github.com/KSPP/linux/issues/89
    
    Upstream-Status: Inappropriate
    
    (cherry picked from commit eaa27e97ee37211f67ff9f81df4288027a576ced)
    (cherry picked from commit 56def939dad2b08529c5a530710aeae5d72e61c9)
    (cherry picked from commit d6ed9f9f5264f2e97559428502bc2606b379233c)
    
    Conflicts:
    	drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.h
    (cherry picked from commit 11e22207163ea7e14ff8d7703670554d8a58aee0)
    Signed-off-by: Luan Rafael Carneiro <luan.r@variscite.com>
    Signed-off-by: FrancescoFerraro <francesco.f@variscite.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/of.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/of.c
@@ -78,6 +78,7 @@ int brcmf_of_probe(struct device *dev, enum brcmf_bus_type bus_type,
 	int err;
 	u32 irqf;
 	u32 val;
+	const char * domain;
 
 	/* Apple ARM64 platforms have their own idea of board type, passed in
 	 * via the device tree. They also have an antenna SKU parameter
@@ -132,6 +133,10 @@ int brcmf_of_probe(struct device *dev, enum brcmf_bus_type bus_type,
 
 	if (bus_type != BRCMF_BUSTYPE_SDIO)
 		return 0;
+
+	/* Laird - Get regdomain/country code string if it exists */
+	if (of_property_read_string(np, "laird,regdomain", &domain) == 0)
+		strscpy(settings->regdomain, domain, BRCMF_REGDOMAIN_LEN);
 
 	if (of_property_read_u32(np, "brcm,drive-strength", &val) == 0)
 		sdio->drive_strength = val;
