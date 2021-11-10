04af79560914cbda8a896aeccaeada50fdbcfc5a
mohit <mohit.p@variscite.com>
2021-11-09 17:34:17 -0800

    imx8mm_var_dart: Changes required to compile sn65dsi83 with 5.10.52

--- drivers/gpu/drm/bridge/sn65dsi83/sn65dsi83_drv.c
+++ drivers/gpu/drm/bridge/sn65dsi83/sn65dsi83_drv.c
@@ -10,7 +10,9 @@
 #include <linux/of_graph.h>
 #include <linux/slab.h>
 
-#include <drm/drmP.h>
+#include <linux/delay.h>
+#include <drm/drm_bridge.h>
+#include <drm/drm_print.h>
 #include <drm/drm_atomic.h>
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_edid.h>
