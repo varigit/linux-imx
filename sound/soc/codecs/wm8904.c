8eaa71dc79ac5df608a46c9d5963107da3bc8c3e
Nate Drude <nate.d@variscite.com>
2021-12-07 11:26:10 -0600

    wm8904: Initialize ADC 128x OSR Switch control to default value
    This fixes the following error when writing R198/0xc6
    (e.g. alsactl restore -c 0 -f 123.conf) while wm8904 is powered down
    and regcache_cache_only = true:
    
    wm8904 0-001a: 2ASoC: error at snd_soc_component_update_bits on wm8904.0-001a: -16
    
    Signed-off-by: Nate Drude <nate.d@variscite.com>
    (cherry picked from commit 0af4a53a68c2394c4c03e62d41d76c54155ff7dd)
    (cherry picked from commit 23287a3650e79e737b554ff90138777d93fba36d)
    (cherry picked from commit 86dd75969e4af21628f7d6abd37f34fd261ebcdc)
    (cherry picked from commit 3a93a9d3c2f55f17aebf8618533eee70e4e7e0f5)
    
    Conflicts:
    	sound/soc/codecs/wm8904.c

--- sound/soc/codecs/wm8904.c
+++ sound/soc/codecs/wm8904.c
@@ -2434,6 +2434,9 @@ static int wm8904_i2c_probe(struct i2c_client *i2c)
 	regmap_update_bits(wm8904->regmap, WM8904_BIAS_CONTROL_0,
 			    WM8904_POBCTRL, 0);
 
+	/* Use default value for ADC Oversampling Ratio (OSR) */
+	regmap_update_bits(wm8904->regmap, WM8904_ADC_TEST_0, 0xffff, 0);
+
 	/* Fill the cache for the ADC test register */
 	regmap_read(wm8904->regmap, WM8904_ADC_TEST_0, &val);
 
