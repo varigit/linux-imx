5331093d8106dddb5997a0cde4db6d723b18bf78
Nate Drude <nate.d@variscite.com>
2022-03-30 17:08:33 -0500

    net: fec: imx8mq: register ADIN1300 PHY fixup to configure rgmii
    Configure rgmii per phy-mode property of device tree.
    
    Signed-off-by: Nate Drude <nate.d@variscite.com>
    (cherry picked from commit 40628fe12c64c3560cdbd7daed8aa16fdacdefbf)

--- drivers/net/ethernet/freescale/fec_fixup.c
+++ drivers/net/ethernet/freescale/fec_fixup.c
@@ -23,6 +23,14 @@
 
 #define PHY_ID_AR8031   0x004dd074
 
+#define PHY_ID_ADIN1300			0x0283bc30
+#define ADIN1300_MII_EXT_REG_PTR	0x0010
+#define ADIN1300_MII_EXT_REG_DATA	0x0011
+#define ADIN1300_GE_RGMII_CFG_REG	0xff23
+#define ADIN1300_GE_RGMII_RXID_EN	BIT(2)
+#define ADIN1300_GE_RGMII_TXID_EN	BIT(1)
+#define ADIN1300_GE_RGMII_EN		BIT(0)
+
 #define IMX8QM_FUSE_MAC0_WORD0		452
 #define IMX8QM_FUSE_MAC0_WORD1		453
 #define IMX8QM_FUSE_MAC1_WORD0		454
@@ -62,6 +70,13 @@ static struct imx_fuse_mac_addr imx8_fuse_mapping[] = {
 	}
 };
 
+static int cpu_is_mx8mq(void)
+{
+	int ret;
+	ret = of_machine_is_compatible("fsl,imx8mq");
+	return ret;
+}
+
 static int ar8031_phy_fixup(struct phy_device *dev)
 {
 	u16 val;
@@ -85,13 +100,83 @@ static int ar8031_phy_fixup(struct phy_device *dev)
 	return 0;
 }
 
+static u16 adin_ext_read(struct phy_device *phydev, const u32 regnum) {
+	u16 val;
+
+	phy_write(phydev, ADIN1300_MII_EXT_REG_PTR, regnum);
+	val = phy_read(phydev, ADIN1300_MII_EXT_REG_DATA);
+
+	phydev_dbg(phydev, "adin_ext_read: adin1300@0x%x 0x%x=0x%x\n", phydev->mdio.addr, regnum, val);
+
+	return val;
+}
+
+static int adin_ext_write(struct phy_device *phydev, const u32 regnum, const u16 val) {
+
+	phydev_dbg(phydev, "adin_ext_write: adin1300@0x%x 0x%x=0x%x\n", phydev->mdio.addr, regnum, val);
+
+	phy_write(phydev, ADIN1300_MII_EXT_REG_PTR, regnum);
+
+	return phy_write(phydev, ADIN1300_MII_EXT_REG_DATA, val);
+}
+
+
+static int adin_config_rgmii_mode(struct phy_device *phydev)
+{
+	u16 val;
+
+	val = adin_ext_read(phydev, ADIN1300_GE_RGMII_CFG_REG);
+
+	if (!phy_interface_is_rgmii(phydev)) {
+		/* Disable RGMII */
+		val &= ~ADIN1300_GE_RGMII_EN;
+		return adin_ext_write(phydev, ADIN1300_GE_RGMII_CFG_REG, val);
+	}
+
+	/* Enable RGMII */
+	val |= ADIN1300_GE_RGMII_EN;
+
+	/* Enable / Disable RGMII RX Delay */
+	if (phydev->interface == PHY_INTERFACE_MODE_RGMII_ID ||
+	    phydev->interface == PHY_INTERFACE_MODE_RGMII_RXID) {
+		val |= ADIN1300_GE_RGMII_RXID_EN;
+	} else {
+		val &= ~ADIN1300_GE_RGMII_RXID_EN;
+	}
+
+	/* Enable / Disable RGMII RX Delay */
+	if (phydev->interface == PHY_INTERFACE_MODE_RGMII_ID ||
+	    phydev->interface == PHY_INTERFACE_MODE_RGMII_TXID) {
+		val |= ADIN1300_GE_RGMII_TXID_EN;
+	} else {
+		val &= ~ADIN1300_GE_RGMII_TXID_EN;
+	}
+
+	return adin_ext_write(phydev, ADIN1300_GE_RGMII_CFG_REG, val);
+}
+
+static int adin1300_phy_fixup(struct phy_device *dev)
+{
+	dev_info(&dev->mdio.dev, "adin1300@0x%x\n", dev->mdio.addr);
+	return adin_config_rgmii_mode(dev);
+}
+
 void fec_enet_register_fixup(struct net_device *ndev)
 {
 	struct fec_enet_private *fep = netdev_priv(ndev);
+	static int adin1300_registered = 0;
 	int err;
 
 	if (!IS_BUILTIN(CONFIG_PHYLIB))
 		return;
+
+	if (!adin1300_registered && cpu_is_mx8mq()) {
+		err = phy_register_fixup_for_uid(PHY_ID_ADIN1300, 0xffffffff,
+							adin1300_phy_fixup);
+		if (err)
+			netdev_info(ndev, "Cannot register adin1300 PHY board fixup\n");
+		adin1300_registered = 1;
+	}
 
 	if (fep->fixups & FEC_QUIRK_AR8031_FIXUP) {
 		static int ar8031_registered = 0;
