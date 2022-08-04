b834e946c54e9c17479aa88a3c03a21620d23e50
Nate Drude <nate.d@variscite.com>
2022-08-04 10:37:04 -0500

    net: stmmac: Fix system hang after interface goes down
    When stmmac MDIO bus is shared with other network interface the
    system hangs after stmmac interface goes down. This is because
    all clocks are shut down and MDIO register access from another
    interface causes the hang. Fix the problem by enabling the clocks
    in stmmac MDIO access routines.
    
    (cherry picked from commit a098cb8071e4798248a007d872cd4954e75b2de1)
    
    # Conflicts:
    #	drivers/net/ethernet/stmicro/stmmac/stmmac_mdio.c
    
    Signed-off-by: Nate Drude <nate.d@variscite.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_mdio.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_mdio.c
@@ -228,6 +228,7 @@ static int stmmac_mdio_read(struct mii_bus *bus, int phyaddr, int phyreg)
 	u32 value = MII_BUSY;
 	int data = 0;
 	u32 v;
+	int ret;
 
 	data = pm_runtime_get_sync(priv->device);
 	if (data < 0) {
@@ -254,6 +255,10 @@ static int stmmac_mdio_read(struct mii_bus *bus, int phyaddr, int phyreg)
 		}
 	}
 
+	ret = stmmac_bus_clks_config(priv, true);
+	if (ret)
+		return ret;
+
 	if (readl_poll_timeout(priv->ioaddr + mii_address, v, !(v & MII_BUSY),
 			       100, 10000)) {
 		data = -EBUSY;
@@ -271,6 +276,8 @@ static int stmmac_mdio_read(struct mii_bus *bus, int phyaddr, int phyreg)
 
 	/* Read the data from the MII data register */
 	data = (int)readl(priv->ioaddr + mii_data) & MII_DATA_MASK;
+
+	stmmac_bus_clks_config(priv, false);
 
 err_disable_clks:
 	pm_runtime_put(priv->device);
@@ -325,6 +332,10 @@ static int stmmac_mdio_write(struct mii_bus *bus, int phyaddr, int phyreg,
 		value |= MII_WRITE;
 	}
 
+	ret = stmmac_bus_clks_config(priv, true);
+	if (ret)
+		return ret;
+
 	/* Wait until any existing MII operation is complete */
 	if (readl_poll_timeout(priv->ioaddr + mii_address, v, !(v & MII_BUSY),
 			       100, 10000)) {
@@ -339,6 +350,8 @@ static int stmmac_mdio_write(struct mii_bus *bus, int phyaddr, int phyreg,
 	/* Wait until any existing MII operation is complete */
 	ret = readl_poll_timeout(priv->ioaddr + mii_address, v, !(v & MII_BUSY),
 				 100, 10000);
+
+	stmmac_bus_clks_config(priv, false);
 
 err_disable_clks:
 	pm_runtime_put(priv->device);
