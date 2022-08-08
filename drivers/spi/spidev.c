684fee1bc6c9a49b64b3a00a30b02e46f412b4b9
Nate Drude <nate.d@variscite.com>
2022-08-08 11:00:52 -0500

    spidev: Add "var,spidev" compatible string to silence warning
    (cherry picked from commit 470e253f576286194143c5045fc66e764a6dfbdd)
    
    Signed-off-by: Nate Drude <nate.d@variscite.com>

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -691,6 +691,7 @@ static const struct spi_device_id spidev_spi_ids[] = {
 	{ .name = "m53cpld" },
 	{ .name = "spi-petra" },
 	{ .name = "spi-authenta" },
+	{ .name = "spidev" },
 	{},
 };
 MODULE_DEVICE_TABLE(spi, spidev_spi_ids);
@@ -717,6 +718,7 @@ static const struct of_device_id spidev_dt_ids[] = {
 	{ .compatible = "menlo,m53cpld", .data = &spidev_of_check },
 	{ .compatible = "cisco,spi-petra", .data = &spidev_of_check },
 	{ .compatible = "micron,spi-authenta", .data = &spidev_of_check },
+	{ .compatible = "var,spidev", .data = &spidev_of_check },
 	{},
 };
 MODULE_DEVICE_TABLE(of, spidev_dt_ids);
