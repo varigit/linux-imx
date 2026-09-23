0c28fa6f80785d89f74aa62aaf750219f6e5d7e7
Luan Rafael Carneiro <luan.r@variscite.com>
2026-09-23 10:46:59 -0300

    Revert "ov5640: fix pixel rate and link frequency calculation"
    This reverts commit 67ffe5a6540cc63af8d3fbb6a54b1cef6b17db27.
    
    Upstream-Status: Inappropriate [Variscite specific]
    
    Signed-off-by: Luan Rafael Carneiro <luan.r@variscite.com>

--- drivers/media/i2c/ov5640.c
+++ drivers/media/i2c/ov5640.c
@@ -1531,7 +1531,7 @@ static int ov5640_set_mipi_pclk(struct ov5640_dev *sensor)
 	 * 2 * sample_period = (mipi_clk * 2 * num_lanes / bpp) * (bpp / 8) / 2
 	 */
 	num_lanes = sensor->ep.bus.mipi_csi2.num_data_lanes;
-	sample_rate = (link_freq * num_lanes * 2) / 16;
+	sample_rate = (link_freq * mipi_div * num_lanes * 2) / 16;
 	pclk_period = 2000000000UL / sample_rate;
 
 	/* Program the clock tree registers. */
@@ -2926,6 +2926,18 @@ static int ov5640_update_pixel_rate(struct ov5640_dev *sensor)
 
 	sensor->current_link_freq = link_freq;
 
+	/*
+	 * Higher link rates require the clock tree to be programmed with
+	 * 'mipi_div' = 1; this has the effect of halving the actual output
+	 * pixel rate in the MIPI domain.
+	 *
+	 * Adjust the pixel rate and link frequency control value to report it
+	 * correctly to userspace.
+	 */
+	if (link_freq > OV5640_LINK_RATE_MAX) {
+		pixel_rate /= 2;
+		link_freq /= 2;
+	}
 	sensor->current_pixel_rate = pixel_rate;
 
 	for (i = 0; i < ARRAY_SIZE(ov5640_csi2_link_freqs); ++i) {
